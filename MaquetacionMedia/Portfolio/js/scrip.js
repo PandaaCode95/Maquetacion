531b65e503fd3b48cc5504504eb3c91a7e037e7a
PandaaCode95 <alvaro.amairal@gmail.com>
2022-05-25 16:14:49 +0200

    Subida de portfolio sin terminar, ig landing e ig feed

---
+++ MaquetacionMedia/Portfolio/js/scrip.js
@@ -0,0 +1,5 @@
+window.addEventListener('scroll', function(){
+    let animacion = document.getElementById('movbg');
+    let positionObj1 = animacion.getBoundingClientRect().top;
+   return positionObj1
+})
